20ff545b5959be985595aa8c0e098dc707da0d56
AstroKiR <eu1kp@yahoo.com>
2020-10-03 12:16:35 +0300

    config from object

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,11 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-
-import os
-
-
-basedir = os.path.abspath(os.path.dirname(__file__))
+from config import Config
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'Thisissupposedtobesecret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'test.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config.from_object(Config)
 bootstrap = Bootstrap(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
